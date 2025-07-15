42f3e3c9dc3402e2a5d948666e1b787b9fbea373
Hihiz <public@mail.ru>
2025-07-15 17:48:43 +0300

    Добавить тест создания марки

---
+++ CarApp.Tests/Brand/Repository/CreateBrandTest.cs
@@ -0,0 +1,18 @@
+﻿using CarApp.Domain.Entities;
+
+namespace CarApp.Tests.Brand.Repository
+{
+    public class CreateBrandTest : BaseIntegrationTest
+    {
+        [Fact]
+        public async Task CreateBrandAsyncTest()
+        {
+            await brandRepository.CreateBrandAsync(new BrandEntity()
+            {
+                Title = "New brand",
+                ManufacturerId = 3,
+                Description = "New brand description"
+            });
+        }
+    }
+}
