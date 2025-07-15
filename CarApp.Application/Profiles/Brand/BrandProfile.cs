cafea4482eb8192d1c2c9782f3e4ccf32db9a8f4
Hihiz <public@mail.ru>
2025-07-15 17:46:23 +0300

    Добавить профиль маппера

---
+++ CarApp.Application/Profiles/Brand/BrandProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using CarApp.Application.Dto.Input.Brand;
+using CarApp.Application.Dto.Output.Brand;
+using CarApp.Domain.Entities;
+
+namespace CarApp.Application.Profiles.Brand
+{
+    /// <summary>
+    /// Класс конфигурации маппера для профиля марки автомобиля.
+    /// </summary>
+    public class BrandProfile : Profile
+    {
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        public BrandProfile()
+        {
+            CreateMap<CreateBrandInput, BrandEntity>();
+            CreateMap<BrandEntity, BrandCreatedOutput>();
+        }
+    }
+}
