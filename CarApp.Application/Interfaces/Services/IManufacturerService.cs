f0bad783245e3c6d48229a1849c65dd317e19705
Hihiz <public@mail.ru>
2025-07-15 17:46:33 +0300

    Добавить интерфейс сервиса

---
+++ CarApp.Application/Interfaces/Services/IManufacturerService.cs
@@ -0,0 +1,31 @@
+﻿using CarApp.Application.Dto.Input.Manufacturer;
+using CarApp.Application.Dto.Output.Manufacturer;
+using CarApp.Domain.Entities;
+
+namespace CarApp.Application.Interfaces.Services
+{
+    /// <summary>
+    /// Интерфейс сервиса производителей автомобилей.
+    /// </summary>
+    public interface IManufacturerService
+    {
+        /// <summary>
+        /// Метод получает список производителей автомобилей.
+        /// </summary>
+        /// <returns>Список производителей.</returns>
+        Task<IEnumerable<ManufacturerOutput>> GetManufacturersAsync();
+
+        /// <summary>
+        /// Метод создает производителя автомобиля.
+        /// </summary>
+        /// <param name="createManufacturerInput">Входная модель.</param>
+        /// <returns>Созданный производитель.</returns>
+        Task<ManufacturerCreatedOutput> CreateManufacturerAsync(CreateManufacturerInput createManufacturerInput);
+
+        /// <summary>
+        /// Метод удаляет производителя автомобиля.
+        /// </summary>
+        /// <param name="manufacturerId">Id производителя автомобиля.</param>
+        Task RemoveManufacturerByIdAsync(int manufacturerId);
+    }
+}
