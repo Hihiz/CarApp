afd4b39db96504032f6b9bdbb27b6730be92d203
Hihiz <public@mail.ru>
2025-07-15 17:45:33 +0300

    Добавить классы сущностей

---
+++ CarApp.Domain/Entities/BrandEntity.cs
@@ -0,0 +1,32 @@
+﻿namespace CarApp.Domain.Entities;
+
+/// <summary>
+/// Класс марки автомобиля сопоставляется с таблицей public.Brand.
+/// </summary>
+public class BrandEntity
+{
+    /// <summary>
+    /// PK.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Название марки автомобиля.
+    /// </summary>
+    public string Title { get; set; }
+
+    /// <summary>
+    /// FK Id производителя автомобиля.
+    /// </summary>
+    public int ManufacturerId { get; set; }
+
+    /// <summary>
+    /// Навигационное свойство.
+    /// </summary>
+    public ManufacturerEntity? Manufacturer { get; set; }
+
+    /// <summary>
+    /// Описание марки автомобиля.
+    /// </summary>
+    public string? Description { get; set; }
+}
---
+++ CarApp.Domain/Entities/ManufacturerEntity.cs
@@ -0,0 +1,22 @@
+﻿namespace CarApp.Domain.Entities;
+
+/// <summary>
+/// Класс производителя автомобиля сопоставляется с таблицей public.Manufacturer.
+/// </summary>
+public class ManufacturerEntity
+{
+    /// <summary>
+    /// PK.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Название производителя автомобиля.
+    /// </summary>
+    public string Title { get; set; }
+
+    /// <summary>
+    /// Список марок автомобилей, которые связанные с производителем.
+    /// </summary>
+    public ICollection<BrandEntity> Brands { get; set; } = new List<BrandEntity>();
+}
