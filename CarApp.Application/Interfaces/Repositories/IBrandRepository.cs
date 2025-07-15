8e9eda7aec8fe79853ab6fd325d73a317f6c5ace
Hihiz <public@mail.ru>
2025-07-15 17:46:05 +0300

    Добавить интерфейс репозиториев

---
+++ CarApp.Application/Interfaces/Repositories/IBrandRepository.cs
@@ -0,0 +1,30 @@
+﻿using CarApp.Application.Dto.Output.Brand;
+using CarApp.Domain.Entities;
+
+namespace CarApp.Application.Interfaces.Repositories
+{
+    /// <summary>
+    /// Интерфейс репозитория марок автомобилей.
+    /// </summary>
+    public interface IBrandRepository
+    {
+        /// <summary>
+        /// Метод получает список марок автомобилей, по Id производителя.
+        /// </summary>
+        /// <param name="manufacturerId">Id производителя.</param>
+        /// <returns>Список марок.</returns>
+        Task<IEnumerable<BrandOutput>> GetBrandsByManufacturerIdAsync(int manufacturerId);
+
+        /// <summary>
+        /// Метод создает марку автомобиля.
+        /// </summary>
+        /// <param name="brandEntity">Модель марки автомобиля.</param>
+        Task CreateBrandAsync(BrandEntity brandEntity);
+
+        /// <summary>
+        /// Метод удаляет марку автомобиля по Id.
+        /// </summary>
+        /// <param name="brandId">Id марки.</param>
+        Task RemoveBrandByIdAsync(int brandId);
+    }
+}
---
+++ CarApp.Application/Interfaces/Repositories/IManufacturerRepository.cs
@@ -0,0 +1,29 @@
+﻿using CarApp.Application.Dto.Output.Manufacturer;
+using CarApp.Domain.Entities;
+
+namespace CarApp.Application.Interfaces.Repositories
+{
+    /// <summary>
+    /// Интерфейс репозитория производителей автомобилей.
+    /// </summary>
+    public interface IManufacturerRepository
+    {
+        /// <summary>
+        /// Метод получает список производителей автомобилей.
+        /// </summary>
+        /// <returns>Список производителей.</returns>
+        Task<IEnumerable<ManufacturerOutput>> GetManufacturersAsync();
+
+        /// <summary>
+        /// Метод создает производителя автомобиля.
+        /// </summary>
+        /// <param name="manufacturerEntity">Модель производителя.</param>
+        Task CreateManufacturerAsync(ManufacturerEntity manufacturerEntity);
+
+        /// <summary>
+        /// Метод удаляет производителя автомобиля.
+        /// </summary>
+        /// <param name="manufacturerId">Id производителя автомобиля.</param>
+        Task RemoveManufacturerByIdAsync(int manufacturerId);
+    }
+}
