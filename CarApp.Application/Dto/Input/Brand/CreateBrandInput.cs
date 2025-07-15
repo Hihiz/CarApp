2f3c0080c59de843c888dad86a1ff8f56f358792
Hihiz <public@mail.ru>
2025-07-15 17:47:22 +0300

    Добавить входную модель

---
+++ CarApp.Application/Dto/Input/Brand/CreateBrandInput.cs
@@ -0,0 +1,23 @@
+﻿namespace CarApp.Application.Dto.Input.Brand
+{
+    /// <summary>
+    /// Класс входной модели создания марки.
+    /// </summary>
+    public class CreateBrandInput
+    {
+        /// <summary>
+        /// Название марки автомобиля.
+        /// </summary>
+        public string? Title { get; set; }
+
+        /// <summary>
+        /// Id производителя автомобиля.
+        /// </summary>
+        public int ManufacturerId { get; set; }
+
+        /// <summary>
+        /// Описание марки автомобиля.
+        /// </summary>
+        public string? Description { get; set; }
+    }
+}
