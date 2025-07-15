50e5abe8b2e0effd5b050746c058103c36fa7cb4
Hihiz <public@mail.ru>
2025-07-15 17:47:11 +0300

    Добавить выходную модель

---
+++ CarApp.Application/Dto/Output/Brand/BrandOutput.cs
@@ -0,0 +1,23 @@
+﻿namespace CarApp.Application.Dto.Output.Brand
+{
+    /// <summary>
+    /// Класс выходной модели марки автомобиля.
+    /// </summary>
+    public class BrandOutput
+    {
+        /// <summary>
+        /// Id марки.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Название марки автомобиля.
+        /// </summary>
+        public string? Title { get; set; }
+
+        /// <summary>
+        /// Описание марки автомобиля.
+        /// </summary>
+        public string? Description { get; set; }
+    }
+}
